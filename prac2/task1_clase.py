f1f75845114b927980845226d03c3e3138eba3ec
Daniu13 <daniel.lozano1@udea.edu.co>
2026-02-27 17:31:51 -0500

    graficar funcion

--- prac2/task1_clase.py
+++ prac2/task1_clase.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def impulso(n0, n1, n2):
     n = np.arange(n1, n2 + 1)
@@ -17,5 +18,18 @@ def rampa(n0, n1, n2):
                        np.arange(0, len(x[x != 0]), dtype=int)])
     return x, n
 
+def graficar(x, n):
+    fig, axs = plt.subplots(1, 1, figsize=(5, 4))
+    axs.plot(n, x, 'b-', label="Sn")
+    axs.set_title('Señales Sn y tn')
+    axs.set_xlabel('n')
+    axs.set_ylabel('func')
+    axs.legend()
+
+    plt.tight_layout()
+    plt.show()
+    return None
+
 results = rampa(0, -10, 10)
 print(results[0])
+graficar(results[0], results[1])
