f4332780605db459d8a2208e3edec8f812920a80
Daniu13 <daniel.lozano1@udea.edu.co>
2026-03-05 20:28:18 -0500

    punto a

--- prac2/task1_clase.py
+++ prac2/task1_clase.py
@@ -1,6 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def graficar(x, n):
+    fig, axs = plt.subplots(1, 1, figsize=(5, 4))
+    axs.stem(n, x, 'b-', label="Sn")
+    axs.set_title('Señales Sn y tn')
+    axs.set_xlabel('n')
+    axs.set_ylabel('func')
+    axs.legend()
+
+    plt.tight_layout()
+    plt.show()
+    return None
+
+#FUNCIONES QUE NOS PIDIERON
+
 def impulso(n0, n1, n2):
     n = np.arange(n1, n2 + 1)
     x = (n == n0).astype(int)
@@ -18,18 +32,22 @@ def rampa(n0, n1, n2):
                        np.arange(0, len(x[x != 0]), dtype=int)])
     return x, n
 
-def graficar(x, n):
-    fig, axs = plt.subplots(1, 1, figsize=(5, 4))
-    axs.stem(n, x, 'b-', label="Sn")
-    axs.set_title('Señales Sn y tn')
-    axs.set_xlabel('n')
-    axs.set_ylabel('func')
-    axs.legend()
+def exponencial(n0, n1, n2, sigma, omega_null):
+    n = np.arange(n1, n2 + 1)
+    x = np.exp((sigma + 1j * omega_null) * n)
+    return x, n
 
-    plt.tight_layout()
-    plt.show()
-    return None
+def sinusoidal(n0, n1, n2, omega_null, theta_null):
+    n = np.arange(n1, n2 + 1)
+    x = np.sin(omega_null * n + theta_null)
+    return x, n
 
-results = rampa(0, -10, 10)
-print(results[0])
-graficar(results[0], results[1])
+
+#5
+#A
+impulsos = [(3, 1), (5, 3), (3, 2), (3, 0), (1, 0)]
+x1 = sum(amp * impulso(n0, -20, 20)[0] for amp, n0 in impulsos)
+n = impulso(0, -20, 20)[1]
+graficar(x1, n)
+
+#B
