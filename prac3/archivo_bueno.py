3dc1b4971f35a90ff1ef5ba2af525383a82710c8
Daniu13 <daniel.lozano1@udea.edu.co>
2026-03-13 17:34:12 -0500

    todo

---
+++ prac3/archivo_bueno.py
@@ -0,0 +1,44 @@
+from scipy.stats import mannwhitneyu
+from statsmodels.tsa.stattools import adfuller
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def rms(signal):
+    n = np.shape(signal)[0]
+    rms = np.sqrt(1 / n * np.sum(signal ** 2))
+    return rms
+
+def graficar(t, signal, signal2):
+    fig, axs = plt.subplots(1, 1, figsize=(10, 5))
+    axs.plot(t, signal)
+    axs.plot(t, signal2, '-r')
+    axs.set_title('Señal')
+    axs.set_xlabel('t')
+    axs.set_ylabel('Amplitud')
+    axs.grid()
+    axs.legend()
+
+    plt.tight_layout()
+    plt.show()
+    return None
+
+fs = 1024 #Hz
+signals = sio.loadmat(r'C:\Users\soportedrai\Documents\Python Scripts\daniel_senales\Lab_biosenales1\prac3\signals.mat') # cambiar para colab
+print(signals.keys())
+# 'ECG_asRecording'
+# 'EMG_asRecording1'
+# 'EMG_asRecording2'
+
+ecg = signals['ECG_asRecording']
+ecg_filtrada = signals['ECG_filtered']
+
+emg = signals['EMG_asRecording1']
+
+vec_tiempo = np.arange(0, len(ecg[0]))
+print(len(ecg[0]))
+print(vec_tiempo)
+
+graficar(vec_tiempo, ecg[0], ecg_filtrada[0])
+#graficar(vec_tiempo, ecg_filtrada[0])
